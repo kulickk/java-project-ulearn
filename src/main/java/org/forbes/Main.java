daee4055f6f781e9baa8b2a2b9b68283ee40229a
kulickk <lilrik1328@gmail.com>
2024-12-17 12:21:39 +0500

    added Main.java

--- src/main/java/org/forbes/Main.java
+++ src/main/java/org/forbes/Main.java
@@ -1,9 +1,26 @@
 package org.forbes;
 
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        DataBase db = new DataBase();
+        db.connect();
+        db.dropTable();
+        db.createTable();
+
+        List<Person> people = CSVParser.readCsv();
+
+        db.insertPeople(people);
+
+        ForbesChart.generateChart(db.getTotalNetWorthByCountry()); // 1 задача
+
+        System.out.println("Самый молодой миллиардер из Франции, капитал которого превышает 10 млрд: "
+                + db.getYoungestBillionaire()); // 2 задача
+
+        System.out.println("Имя и компания бизнесмена из США, имеющего самый большой капитал в сфере Energy: "
+                + db.getBiggestCapitalInEnergy()); // 3 задача
+
     }
 }
-
